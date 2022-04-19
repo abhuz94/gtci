40f024a9138789326d18803f1fa0de82b224f4e0
abid hussein <101010301+abhuz94@users.noreply.github.com>
2022-04-19 11:30:53 +0800

    max-sum-subarray

---
+++ src/sliding-window/max-sum-subarray.js
@@ -0,0 +1,13 @@
+export default function maxSumSubarray(arr = []) {
+  let maxTillHere = Number.NEGATIVE_INFINITY;
+  let maxTillNow = 0;
+
+  for (let i = 0; i < arr.length; i += 1) {
+    maxTillNow += arr[i];
+    maxTillHere = Math.max(maxTillNow, maxTillHere);
+
+    if (maxTillNow < 0) { maxTillNow = 0; }
+  }
+
+  return maxTillHere;
+}
---
+++ src/sliding-window/max-sum-subarray.test.js
@@ -0,0 +1,5 @@
+import maxSumSubarray from './max-sum-subarray';
+
+test('Maximum Subarray', () => {
+  expect(maxSumSubarray([-2, 1, -3, 4, -1, 2, 1, -5, 4])).toEqual(6);
+});
